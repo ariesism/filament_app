5115d1a00d6cb81c8027114fbf002b003eaf2e51
Edison <ariesism@gmail.com>
2026-03-11 23:31:56 +0800

    Add Product model and migration: define fillable attributes and create products table

---
+++ app/Models/Product.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+        'name',
+        'sku',
+        'description',
+        'price',
+        'stock_quantity',
+        'category',
+        'image_url',
+        'is_active',
+    ];
+}
